abaf174469afee508d51eb2fd4223b1f2ec540ac
Juan Camilo Jiménez Rojas <jcjimenezr@eafit.edu.co>
2020-05-01 11:49:57 -0500

    second

--- webPage/src/model/routes/shop.js
+++ webPage/src/model/routes/shop.js
@@ -1,12 +1,16 @@
 const dbConnection = require('../../model/dbConnection')
 module.exports = (app) => {
     const connestion = dbConnection();
-
-    app.get('/',(req,res) => {
-        connestion.query('SELECT * FROM Vendedornpm install bootstrap', (err, result) => {
-            res.render('../view/shop.ejs',{
+    app.get('/products',(req,res) => {
+        connestion.query('SELECT * FROM Vendedor', (err, result) => {
+            console.log(result)
+            res.render('../view/products.ejs',{
                 shop: result
             });
         })
+    });
+
+    app.get('/shop',(req,res) => {
+            res.render('../view/shop.ejs');
     });
 }
