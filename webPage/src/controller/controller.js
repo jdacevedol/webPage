b7bcff3ef204d5cd448fcbe1eb042f6cac87612d
Juan Camilo Jiménez Rojas <jcjimenezr@eafit.edu.co>
2020-05-04 16:33:32 -0500

    Add files via upload

--- webPage/src/controller/controller.js
+++ webPage/src/controller/controller.js
@@ -70,6 +70,59 @@ controller.updateProduct = (req, res) => {
             res.redirect('/products')
         }
     })
-
 }
+
+controller.listshop = (req, res) => {
+    connestion.query('SELECT * from productos', (err, result) => {
+        if(err) {
+            throw err
+        }else {
+            res.render('../../src/view/buy.ejs',{ shop: result });
+        }
+    })
+}
+
+controller.info = (req, res) => {
+    const {id} = req.params;
+    connestion.query(`SELECT * FROM productos WHERE idProductos = ${id}`,
+    (err, result) => {
+        if(err) {
+            throw err
+        }else {
+            console.log(result[0]);
+            res.render('../../src/view/info.ejs',{data: result[0]})
+        }
+    })
+}
+
+controller.loadProfile = (req, res) => {
+    const {id} = req.params;
+    connestion.query(`SELECT * FROM vendedor WHERE Usuario_username = "hi"`,
+    (err, result) => {
+        if(err) {
+            throw err
+        }else {
+            res.render('../../src/view/profile.ejs',{data: result[0]})
+        }
+    })
+}
+
+controller.updateProfile = (req, res) => {
+    const {id} = req.params;
+    const {nombresU, apellidosU, celular, correo} = req.body
+    connestion.query('UPDATE vendedor SET ? WHERE Usuario_username = ?', [{
+        nombresU,
+        apellidosU,
+        celular,
+        correo,
+        Usuario_username: id
+    },id],(err, result) => {      
+        if(err) {
+            throw err
+        }else {
+            res.redirect('/updateProfile')
+        }
+    })
+}
+
 module.exports = controller
--- webPage/src/model/routes/shop.js
+++ webPage/src/model/routes/shop.js
@@ -2,9 +2,7 @@ const dbConnection = require('../../model/dbConnection')
 const controlller = require('../../controller/controller')
 module.exports = (app) => {
     const connestion = dbConnection();
-    app.get('/shop',(req,res) => {
-        res.render('../view/shop.ejs');
-    });
+    app.get('/shop',(req,res) => res.render('../view/shop.ejs'));
 
     app.get('/products',controlller.list)
 
@@ -15,4 +13,12 @@ module.exports = (app) => {
     app.get('/update/:id', controlller.editProducts);
 
     app.post('/update/:id', controlller.updateProduct);
+
+    app.get('/buy',controlller.listshop);
+
+    app.get('/info/:id',controlller.info);
+
+    app.get('/updateProfile', controlller.loadProfile);
+
+    app.post('/updateProfile/:id', controlller.updateProfile);
 }
